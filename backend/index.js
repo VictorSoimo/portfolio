ec29ddb3a439da0a0ebe154b96a8ebb984d89828
Soimo <miranjikarl@gmail.com>
2026-06-07 00:59:11 +0300

    add deploy workflow for backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/api/hello', (req, res) => {
+    res.send('hello world');
+});
+
+app.listen(port, () =>{
+    console.log(`app listening on port ${port}`);
+})
